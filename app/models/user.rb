03c9d3e19d308089d38a1d6cc4301efb97c78208
hishalv <hishalv@outlook.com>
2019-03-21 15:22:59 +0200

    added User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-	
+	validates :username, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: true }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 25 }, uniqueness: { case_sensitive: true }, format: { with: VALID_EMAIL_REGEX }
 end
